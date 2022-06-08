235a3cc0bf8400470d7b6e408141b2de78b7991e
Henry <vividnight@protonmail.com>
2022-06-08 14:31:32 -0400

    finished array 'every' method

--- main.js
+++ main.js
@@ -1,67 +1,65 @@
 // MAP //
-Array.prototype.myMap = function(callbackFn, args) {
-    if(this.length === 0) {
-        return "Array is empty";
-    }
-    let newArray = [];
-    for(let i = 0; i < this.length; i++) {
-        newArray.push(callbackFn(this[i], args));
-    }
-    return newArray;
+Array.prototype.myMap = function (callbackFn, args) {
+  if (this.length === 0) {
+    return "Array is empty";
+  }
+  let newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(callbackFn(this[i], args));
+  }
+  return newArray;
 };
 
 // FILTER //
-Array.prototype.myFilter = function(callbackFn, args) {
-    if(this.length === 0) {
-        return "Array is empty";
+Array.prototype.myFilter = function (callbackFn, args) {
+  if (this.length === 0) {
+    return "Array is empty";
+  }
+  let newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    if (callbackFn(this[i], args)) {
+      newArray.push(this[i]);
     }
-    let newArray = [];
-    for(let i = 0; i < this.length; i++) {
-        if(callbackFn(this[i], args)) {
-            newArray.push(this[i]);
-        }
-    }
-    return newArray;
+  }
+  return newArray;
 };
 
 // SOME //
-Array.prototype.mySome = function(callbackFn) {
-
-};
+Array.prototype.mySome = function (callbackFn) {};
 
 // EVERY //
-Array.prototype.myEvery = function(callbackFn) {
+Array.prototype.myEvery = function (callbackFn) {
+  if (this.length === 0) {
+    return false;
+  }
 
+  for (let i = 0; i < this.length; i++) {
+    if (!callbackFn(this.at(i))) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // REDUCE //
-Array.prototype.myReduce = function(callbackFn) {
-
-};
+Array.prototype.myReduce = function (callbackFn, initialValue) {};
 
 // INCLUDES //
-Array.prototype.myIncludes = function(searchElement) {
-
-};
+Array.prototype.myIncludes = function (searchElement) {};
 
 // INDEXOF //
-Array.prototype.myIndexOf = function(searchElement) {
-
-};
+Array.prototype.myIndexOf = function (searchElement) {};
 
 // LASTINDEXOF //
-Array.prototype.myLastIndexOf = function(searchElement) {
-
-};
+Array.prototype.myLastIndexOf = function (searchElement) {};
 
 // KEYS //
-Object.myKeys = function(object) {
-
-};
+Object.myKeys = function (object) {};
 
 // VALUES //
-Object.myValues = function(object) {
-
-};
+Object.myValues = function (object) {};
 
 //TEST CODE//
+const testArr = [1, 2, 3, 4];
+console.log(testArr.myEvery((ele) => ele > 0));
