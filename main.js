271bed1b467cf74a7f46366f179a5ea8dbd49114
Jason <Jason.Wong47@myhunter.cuny.edu>
2022-06-08 12:45:41 -0400

    added map function

--- main.js
+++ main.js
@@ -1,49 +1,57 @@
 // MAP //
-Array.prototype.myMap = function(callbackFn) {
-    // Place your code here.
-  };
-  
-  // FILTER //
-  Array.prototype.myFilter = function(callbackFn) {
-    // Place your code here.
-  };
-  
-  // SOME //
-  Array.prototype.mySome = function(callbackFn) {
-    // Place your code here.
-  };
-  
-  // EVERY //
-  Array.prototype.myEvery = function(callbackFn) {
-    // Place your code here.
-  };
-  
-  // REDUCE //
-  Array.prototype.myReduce = function(callbackFn) {
-    // Place your code here.
-  };
-  
-  // INCLUDES //
-  Array.prototype.myIncludes = function(searchElement) {
-    // Place your code here.
-  };
-  
-  // INDEXOF //
-  Array.prototype.myIndexOf = function(searchElement) {
-    // Place your code here.
-  };
-  
-  // LASTINDEXOF //
-  Array.prototype.myLastIndexOf = function(searchElement) {
-    // Place your code here.
-  };
-  
-  // KEYS //
-  Object.myKeys = function(object) {
-    // Place your code here.
-  };
-  
-  // VALUES //
-  Object.myValues = function(object) {
-    // Place your code here.
-  };
+Array.prototype.myMap = function(callbackFn, args) {
+    if(this.length === 0) {
+        return "Array is empty";
+    }
+    let newArray = [];
+    for(let i = 0; i < this.length; i++) {
+        newArray.push(callbackFn(this[i], args));
+    }
+    return newArray;
+};
+
+// FILTER //
+Array.prototype.myFilter = function(callbackFn, args) {
+};
+
+// SOME //
+Array.prototype.mySome = function(callbackFn) {
+
+};
+
+// EVERY //
+Array.prototype.myEvery = function(callbackFn) {
+
+};
+
+// REDUCE //
+Array.prototype.myReduce = function(callbackFn) {
+
+};
+
+// INCLUDES //
+Array.prototype.myIncludes = function(searchElement) {
+
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function(searchElement) {
+
+};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function(searchElement) {
+
+};
+
+// KEYS //
+Object.myKeys = function(object) {
+
+};
+
+// VALUES //
+Object.myValues = function(object) {
+
+};
+
+//TEST CODE//
